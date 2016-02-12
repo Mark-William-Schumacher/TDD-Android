d252b6d10b4e5a8e487c851a5ea1315ef77dd54e
Mark Schumacher <905mws@gmail.com>
2016-02-12 13:32:16 -0500

    Mockito Setup and ready for Hello world

---
+++ app/src/androidTest/java/com/example/mobile/skyline_test_driven_development/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.mobile.skyline_test_driven_development;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
